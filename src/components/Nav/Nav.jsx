d9d64adb08df69606cd69e17f62e680f7a97a521
Heng Yang <hengya03@gmail.com>
2023-05-22 15:18:40 -0500

    Modified the Nav.jsx and Nav.css file to have the nav bar be displayed on the left hand side as a column.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Know Your Hours</h2>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
@@ -24,15 +24,19 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
+            <div>
+              <Link className="navLink" to="/user">
+                Home
+              </Link>
+            </div>
+            <div>
+              <Link className="navLink" to="/info">
+                Info Page
+              </Link>
+            </div>
+            <div>
+              <LogOutButton className="navLink" />
+            </div>
           </>
         )}
 
