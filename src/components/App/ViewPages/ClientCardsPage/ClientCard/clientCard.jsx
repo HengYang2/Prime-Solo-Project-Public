01c2f5722502c47d54d9955aee1b31c380acd9da
Heng Yang <hengya03@gmail.com>
2023-05-25 09:28:28 -0500

    Updated clientCard.jsx to show relevant client information on the DOM

--- src/components/App/ViewPages/ClientCardsPage/ClientCard/clientCard.jsx
+++ src/components/App/ViewPages/ClientCardsPage/ClientCard/clientCard.jsx
@@ -1,10 +1,28 @@
 function ClientCard(props) {
 
   const clientInfo = props.clientInfo
+  let endDate = '';
+
+  //Create a function that will check to see if clientInfo.is_still_subscribed == true:
+  //If it is, set 'endDate' = 'Present Day'
+  function isClientStillSubscribed() {
+    if (clientInfo.is_still_subscribed) {
+      endDate = 'Present Day';
+    } else {
+      endDate = clientInfo.end_date;
+    }
+  }
+
+  isClientStillSubscribed();
 
   return (
     <div className="clientCard" key={clientInfo.id}>
-        <h3>{clientInfo.client_initials}</h3>
+        <h2>{clientInfo.client_initials}</h2>
+        <h3>{clientInfo.start_date}</h3>
+        <h3>to</h3>
+        <h3>{endDate}</h3>
+        <p>{clientInfo.client_note}</p>
+
     </div>
   );
 }
