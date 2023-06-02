53b45b467da3b8ef17beca8a70898bbf6c8e7ca2
Heng Yang <hengya03@gmail.com>
2023-06-01 21:13:11 -0500

    Reformatted the dates for EditClientCardRender_Left.jsx

--- src/components/Modals/MainModal/ConditionalRenders/LeftSide/EditClientCardRender_Left/EditClientCardRender_Left.jsx
+++ src/components/Modals/MainModal/ConditionalRenders/LeftSide/EditClientCardRender_Left/EditClientCardRender_Left.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { func } from "prop-types";
 
+
 function EditClientCardRender_Left() {
 
     //Load all the relevant reducers for editing client card:
@@ -13,21 +14,58 @@ function EditClientCardRender_Left() {
 
     const selectedClientCardReducer = useSelector(store => store.selectedClientCardReducer);
 
-    function formatEndDate() {
-        if (selectedClientCardReducer.end_date == null) {
-            return 'Present Day'
+    //This is for displaying the date in mm/dd/yyyy format to the users in the DOM.
+    // const [reformattedStartDate, setReformattedStartDate] = useState('');
+    // const [reformattedEndDate, setReformattedEndDate] = useState('');
+
+
+
+    //Function that will convert date format: yyyy/mm/dd --> mm/dd/yyyy
+    function reformatDate(date) {
+        const yyyy = date[0] + date[1] + date[2] + date[3];
+        const mm = date[5] + date[6];
+        const dd = date[8] + date[9];
+        const reformattedDate = mm + '/' + dd + '/' + yyyy;
+        return reformattedDate;
+    }
+
+    //Function will set the value for reformattedStartDate and reformattedEndDate based
+    //on the second passed in parameter, 'startOrEnd':
+    const handleDate = (date, startOrEnd) => {
+
+        //Reformat Date
+        const reformattedDate = reformatDate(date);
+
+        //Assign it to reformattedStartDate or reformattedEndDate:
+        if (startOrEnd == 'start') {
+            return (reformattedDate);
         } else {
-            return selectedClientCardReducer.end_date
+            return (reformattedDate);
         }
     }
+
+    //Conditionally rendered endDate component:
+    const conRenderedEndDate = () => {
+        if (selectedClientCardReducer.is_still_subscribed == false) {
+            return (
+               handleDate(selectedClientCardReducer.end_date, 'end')
+            )
+        } else {
+            return (
+                'Present Day'
+            )
+        }
+    }
+
+    const reformattedStartDate = handleDate(selectedClientCardReducer.start_date, 'start')
 
     return (
         <>
             <h2 className="clientInitials">{selectedClientCardReducer.client_initials}</h2>
             <div className="timeWithCompany">
-                <h3 className="startDate">{selectedClientCardReducer.start_date}</h3>
+                <h3 className="startDate">{reformattedStartDate}</h3>
                 <h3 className="to">to</h3>
-                <h3 className="endDate">{formatEndDate()}</h3>
+                <h3 className="endDate">{conRenderedEndDate()}</h3>
             </div>
             <p className="clientNote">{selectedClientCardReducer.client_note}</p>
         </>
